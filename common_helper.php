9ea50200762aeef0e36bc9ba7d5df66f0645dd14
kaluo <284474102@qq.com>
2018-07-17 16:17:29 +0800

    新增：获取图片的Base64编码(不支持url)

--- common_helper.php
+++ common_helper.php
@@ -28,6 +28,53 @@ function get_curl_data($url, $param = array())
     curl_close($ch);
     return $res;
 }
+
+/**
+ * 获取图片的Base64编码(不支持url)
+ * @date 2017-02-20 19:41:22
+ *
+ * @param $img_file 传入本地图片地址
+ *
+ * @return string
+ */
+function imgToBase64($img_file) {
+
+    $img_base64 = '';
+    if (file_exists($img_file)) {
+        $app_img_file = $img_file; // 图片路径
+        $img_info = getimagesize($app_img_file); // 取得图片的大小，类型等
+
+        //echo '<pre>' . print_r($img_info, true) . '</pre><br>';
+        $fp = fopen($app_img_file, "r"); // 图片是否可读权限
+
+        if ($fp) {
+            $filesize = filesize($app_img_file);
+            $content = fread($fp, $filesize);
+            $file_content = chunk_split(base64_encode($content)); // base64编码
+
+            switch ($img_info[2]) {
+                //判读图片类型
+
+                case 1: $img_type = "gif";
+                    break;
+                case 2: $img_type = "jpg";
+                    break;
+                case 3: $img_type = "png";
+                    break;
+                case 17: $img_type = "x-icon";
+                    break;
+                default :
+                    return null;
+            }
+            $img_base64 = 'data:image/' . $img_type . ';base64,' . $file_content;//合成图片的base64编码
+
+        }
+        fclose($fp);
+    }
+
+    return $img_base64; //返回图片的base64
+}
+
 /**
  *移动端判断
  * @return bool
