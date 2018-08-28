be6553251fd202e76e587a85257af046a9ad5d2f
carlo <284474102@qq.com>
2018-08-28 23:43:06 +0800

    利用google api生成二维码图片

--- common_helper.php
+++ common_helper.php
@@ -1,5 +1,15 @@
 <?php
-
+    /** *利用google api生成二维码图片
+     * $content：二维码内容参数
+     * $size：生成二维码的尺寸，宽度和高度的值
+     * $lev：可选参数，纠错等级
+     * $margin：生成的二维码离边框的距离
+     */
+    function create_erweima($content, $size = '200', $lev = 'L', $margin= '0') {
+        $content = urlencode($content);
+        $image = '<img src="http://chart.apis.google.com/chart?chs='.$size.'x'.$size.'&amp;cht=qr&chld='.$lev.'|'.$margin.'&amp;chl='.$content.'"  widht="'.$size.'" height="'.$size.'" />';
+        return $image;
+    }
     /**
      * 请求远程数据
      * @param type $url
