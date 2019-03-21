48dd6dad7242b9426620c62ddb09c34f33afb7a3
carlo <284474102@qq.com>
2019-03-21 16:50:03 +0800

    ip

---
+++ ip_test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 蒋炳杰
+ * @create 2019-03-21 16:36:10
+ * @author jiangbingjie<jiangbinjie@i3020.com>
+ */
+require_once 'ip/Ip_location.php';
+$ip_location = new Ip_location();
+$ip = "180.76.6.130";
+$location = $ip_location->getlocation($ip);
+var_dump($location);
